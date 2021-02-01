3cfcb1a4010d945e1279ea9f945a53d0944611e0
Your Name <you@example.com>
2021-02-01 14:23:02 +0530

    01/Feb/2021 - Running Condition

--- src/main/java/com/INCOMMfiles/INCOMMsocketDataProcessor.java
+++ src/main/java/com/INCOMMfiles/INCOMMsocketDataProcessor.java
@@ -2,7 +2,9 @@
  * This class is used to customize the socket programming for the Incomm fep.
  * socketDataReadFormat method is implemented in such a way that the first two bytes in the byte stream is read to 
  * find out the overall length of the request packet.
- * Similarly, the way data is written into the socket is also customized to fit the requirement of the HPS. 
+ * Similarly, the way data is written into the socket is also customized to fit the requirement of the Incomm. 
+ * Please note that in Incomm, when the data length is written into the socket, it will not include the header bytes 
+ * i.e., if data of length 100 should be sent, data length of 100 will be written into socket unlike hps where it will be data length +2
  */
 
 package com.INCOMMfiles;
@@ -23,7 +25,7 @@ public class INCOMMsocketDataProcessor extends BaseSocketDataProcessor {
 	public void socketDataReadFormat() {
 		try {
 			socketDataLength = dataInputStream.readShort();
-			logger.debug("Socket has received data of length: "+socketDataLength);
+			logger.debug("Socket has received data of length: " + socketDataLength);
 		} catch (IOException e) {
 			logger.debug(e.toString());
 		}
@@ -33,7 +35,7 @@ public class INCOMMsocketDataProcessor extends BaseSocketDataProcessor {
 	@Override
 	public void socketDataWriteFormat(String responsePacket) {
 		responsePacket = Initializer.getConverter().toHexString(responsePacket);
-		logger.debug("Converted value of response packet in hex format "+responsePacket);
+		logger.debug("Converted value of response packet in hex format " + responsePacket);
 		try {
 			formattedPacketBytes = responsePacket.getBytes("ISO-8859-1");
 			logger.debug("Response packet successfully converted to byte array");
